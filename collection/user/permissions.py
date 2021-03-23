56904e574ed503cb4f604ae45a29d5d88f839832
JoseGuiFerreira17 <guibsonmouta15@gmail.com>
2021-03-22 23:18:17 -0300

    criaçao da app de usuario

---
+++ collection/user/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework.permissions import BasePermission
+
+
+WRITE_METHOD = ['POST']
+
+
+class IsAuthenticatedOrWriteOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return bool(
+            request.method in WRITE_METHOD or
+            request.user and request.user.is_authenticated
+        )
+        
